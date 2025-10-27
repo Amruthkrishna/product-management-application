3954a40f6edd4e3422cf5a717ff09cc1216d759d
amruthkrishna <amruthrk87@gmail.com>
2025-10-27 14:34:18 +0530

    feat: add Rating enum and Product constructor; make Product immutable

--- src/labs/pm/data/Product.java
+++ src/labs/pm/data/Product.java
@@ -16,6 +16,7 @@
 package labs.pm.data;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import static labs.pm.data.Rating.*;
 /**
  * {@code Product} class represents properties and behaviors of
  * product objects in the Product Management System.
@@ -28,9 +29,25 @@ import java.math.RoundingMode;
  * @author oracle
  */
 public class Product {
-    private int id;
-    private String name;
-    private BigDecimal price;
+    private final int id;
+    private final String name;
+    private final BigDecimal price;
+    private final Rating rating;
+
+    public Product(int id, String name, BigDecimal price, Rating rating) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.rating = rating;
+    }
+    public Product(int id, String name, BigDecimal price){
+        this(id, name, price, NOT_RATED);
+    }
+
+    public Product(){
+        this(0,"No Name",BigDecimal.ZERO);
+    }
+
 
     /**
      * A constant that defines a
@@ -43,25 +60,11 @@ public class Product {
     public int getId() {
         return id;
     }
-
-    public void setId(final int id) {
-        this.id = id;
-    }
-
     public String getName() {
         return name;
     }
-
-    public void setName(final String name) {
-        this.name = name;
-    }
-
     public BigDecimal getPrice() {
         return price;
-    }
-
-    public void setPrice(final BigDecimal price) {
-        this.price = price;
     }
     /**
      * Calculates discount based on a product price and
@@ -71,5 +74,12 @@ public class Product {
      */
     public BigDecimal getDiscount(){
         return price.multiply(DISCOUNT_RATE).setScale(2, RoundingMode.HALF_UP);
+    }
+
+    public Rating getRating() {
+        return rating;
+    }
+    public Product applyRating(Rating newRating){
+        return new Product(id,name,price,newRating);
     }
 }
