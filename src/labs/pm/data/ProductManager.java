c340a4800471360b4dc6e5fceceadf2c6ac295f8
amruthkrishna <amruthrk87@gmail.com>
2025-11-02 22:01:23 +0530

    Implemented product review feature: - Added Review record class to represent product reviews - Added two new review-related methods in Product class - Updated Shop class to support adding and displaying product reviews

--- src/labs/pm/data/ProductManager.java
+++ src/labs/pm/data/ProductManager.java
@@ -14,7 +14,6 @@
  */
 
 package labs.pm.data;
-
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
@@ -22,12 +21,37 @@ import java.time.LocalDate;
  * @author oracle
  **/
 public class ProductManager {
+    private Product product;
+    private Review review;
+
     public Product createProduct(int id, String name, BigDecimal price, Rating rating, LocalDate bestbefore){
-        return new Food(id, name, price, rating, bestbefore);
-    }
-    public Product createProduct(int id, String name, BigDecimal price, Rating rating){
-        return new Drink(id, name, price, rating);
+        product = new Food(id, name, price, rating, bestbefore);
+        return product;
     }
 
+    public Product createProduct(int id, String name, BigDecimal price, Rating rating){
+        product = new Drink(id, name, price, rating);
+        return product;
+    }
+
+    public Product reviewProduct(Product product,Rating rating,String comments){
+        review = new Review(rating, comments);
+        this.product = product.applyRating(rating);
+        return this.product;
+    }
+
+    public void printProductReport(){
+        StringBuilder txt  = new StringBuilder();
+        txt.append(product);
+        txt.append('\n');
+        if(review!=null){
+            txt.append(review);
+        }
+        else{
+            txt.append("Not reviewed");
+        }
+        txt.append('\n');
+        System.out.println(txt);
+    }
 
 }
